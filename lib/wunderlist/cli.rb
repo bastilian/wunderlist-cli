d22eed85df26ff389854ecf9ea6570894a2403e1
Sebastian Gräßl <sebastian@validcode.me>
2013-11-15 19:22:47 +0100

    initial commit

---
+++ lib/wunderlist/cli.rb
@@ -0,0 +1,7 @@
+require "wunderlist/cli/version"
+
+module Wunderlist
+  module Cli
+    # Your code goes here...
+  end
+end
---
+++ lib/wunderlist/cli/version.rb
@@ -0,0 +1,5 @@
+module Wunderlist
+  module Cli
+    VERSION = "0.0.1"
+  end
+end
